e41ccb0b72dddfe712f7a3a7efbdeea715c4040b
codingInSpace <jonathan.grangien@gmail.com>
2014-10-04 14:40:30 +0200

    basic functionality update personnummer (måste vi göra det här på engelska?)

--- pages/edit-profile.php
+++ pages/edit-profile.php
@@ -1,4 +1,16 @@
-<div class="col-sm-12">
-	<div class="page-header">
-		<h1>Redigera konto</h1>
+	<div class="col-sm-12">
+		<div class="page-header">
+			<h1>Redigera konto</h1>
+		</div>
 	</div>
+</div> <!-- .row -->
+
+<div class="row">
+	
+<div class="col-sm-5">
+	<form action="" method="post">
+		<p><input type="text" placeholder="Kontroll: Användarnamn" name="user_name"/></p>
+		<p><input type="text" placeholder="Personnummer" name="ssn" maxlength="10"/></p>
+		<p><input type="submit" name="submit" value="Uppdatera"/></p>
+	</form>
+</div>
--- php/pages/edit-profile.php
+++ php/pages/edit-profile.php
@@ -1,8 +1,30 @@
 <?php
 include_once('php/DBQuery.php');
 
+if(isset($_POST['submit'])) {
+	$user_name = $_POST['user_name']; //user name variable needed for WHERE clause?
+	$ssn = $_POST['ssn'];
+	
+	//necessary?
+	$ssn = mysql_real_escape_string($ssn);
+	$user_name = mysql_real_escape_string($user_name);
 
+	//control
+	echo "ssn = " . $ssn;
 
+	//add later:
+	//if($userName != '' && $password != '' && $ssn != '' && $mail != '' && $name != '' && $lastName != '')
+	//{
+		DBQuery::sql("UPDATE user
+					SET ssn = '$ssn'
+					WHERE user_name = '$user_name'");
 
-
+		?>
+		<script>
+			window.location = "?page=edit-profile";
+		</script>
+		<?php
+	//}
+	
+}
 ?>

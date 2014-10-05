a1e30825aa6533b84d70c2111b0e16d302fa3195
codingInSpace <jonathan.grangien@gmail.com>
2014-10-05 20:16:11 +0200

    added: update ssn, mail, names problem: empty string

--- pages/edit-profile.php
+++ pages/edit-profile.php
@@ -1,6 +1,6 @@
 	<div class="col-sm-12">
 		<div class="page-header">
-			<h1>Redigera konto</h1>
+			<h1>Redigera profil</h1>
 		</div>
 	</div>
 </div> <!-- .row -->
@@ -9,8 +9,11 @@
 	
 <div class="col-sm-5">
 	<form action="" method="post">
-		<p><input type="text" placeholder="Kontroll: Användarnamn" name="user_name"/></p>
-		<p><input type="text" placeholder="Personnummer" name="ssn" maxlength="10"/></p>
+		<p><input type="text" placeholder="Användarnamn" name="user_name"/></p>
+	<!--<p><input type="text" placeholder="Personnummer" name="ssn" maxlength="10"/></p>
+		<p><input type="text" placeholder="Mail" name="mail"/></p>
+		<p><input type="text" placeholder="Namn" name="name"/></p>
+		<p><input type="text" placeholder="Efternamn" name="last_name"/></p> -->
 		<p><input type="submit" name="submit" value="Uppdatera"/></p>
 	</form>
 </div>
--- php/pages/edit-profile.php
+++ php/pages/edit-profile.php
@@ -2,29 +2,53 @@
 include_once('php/DBQuery.php');
 
 if(isset($_POST['submit'])) {
-	$user_name = $_POST['user_name']; //user name variable needed for WHERE clause?
-	$ssn = $_POST['ssn'];
-	
-	//necessary?
-	$ssn = mysql_real_escape_string($ssn);
-	$user_name = mysql_real_escape_string($user_name);
-
+	$user_name = $_POST['user_name'];	//gets empty string wtf
+/*	$ssn = $_POST['ssn'];
+	$mail = $_POST['mail'];
+	$name = $_POST['name'];
+	$lastName = $_POST['last_name'];
+*/	
 	//control
-	echo "ssn = " . $ssn;
+	if(empty($_POST['userName']))
+		echo "empty";
+	else
+		echo "userName = " + $user_name;
+	/*   "ssn = " . $ssn + \n +
+		 "mail = " . $mail + \n +
+		 "name = " . $name + \n +
+		 "lastName = " . $lastName;
+	*/
 
-	//add later:
-	//if($userName != '' && $password != '' && $ssn != '' && $mail != '' && $name != '' && $lastName != '')
-	//{
-		DBQuery::sql("UPDATE user
-					SET ssn = '$ssn'
-					WHERE user_name = '$user_name'");
+
+	/*	necessary?
+	$userName = mysql_real_escape_string($userName);
+	$ssn = mysql_real_escape_string($ssn);
+	$mail = mysql_real_escape_string($mail);
+	$name = mysql_real_escape_string($name);
+	$lastName = mysql_real_escape_string($lastName);	*/
+	
+
+/*
+	//if($userName != '' && $ssn != '' && $mail != '' && $name != '' && $lastName != ''){
+	DBQuery::sql("UPDATE user
+				  SET ssn='$ssn', mail='$mail', name='$name', lastName='$lastName'
+				  WHERE user_name='$userName'");
 
 		?>
 		<script>
 			window.location = "?page=edit-profile";
 		</script>
 		<?php
+*/
 	//}
+	/*
+	else{
+		?>
+		<script>
+		alert("error: empty fields","");
+		</script>
+		<?php
+	}*/
 	
 }
 ?>

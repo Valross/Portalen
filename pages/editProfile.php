f09686d4c84d81c43799f07115aaa794632bfd99
Jonathan <jonathan.grangien@gmail.com>
2014-10-08 18:29:15 +0200

    Added simple labels and current values to field placeholders

--- pages/editProfile.php
+++ pages/editProfile.php
@@ -10,12 +10,12 @@
 <div class="col-sm-5">
 	<form action="" method="post">
 	    <p>Fyll i de fält du vill uppdatera</p>
-	    <p><input type="text" placeholder="Mobilnummer" name="phone_number" maxlength="15" /></p>
-	    <p><input type="text" placeholder="Personnummer" name="ssn" maxlength="10" /></p>
-		<p><input type="text" placeholder="Mail" name="mail" /></p>
-		<p><input type="text" placeholder="Namn" name="name" /></p>
-		<p><input type="text" placeholder="Efternamn" name="last_name" /></p>
-		<p><textarea rows="4" cols="50" placeholder="Beskrivning" name="description" maxlength="150"></textarea></p>
+	    <p>Mobilnummer: <input type="text" placeholder="<?php echo $profileNumber; ?>" name="phone_number" maxlength="15" /></p>
+	    <p>Personnummer: <input type="text" placeholder="<?php echo $profileSsn; ?>" name="ssn" maxlength="10" /></p>
+		<p>Mail: <input type="text" placeholder="<?php echo $profileMail; ?>" name="mail" /></p>
+		<p>Förnamn: <input type="text" placeholder="<?php echo $profileName; ?>" name="name" /></p>
+		<p>Efternamn: <input type="text" placeholder="<?php echo $profileLastName; ?>" name="last_name" /></p>
+		<p>Beskrivning: <textarea rows="4" cols="50" placeholder="<?php echo $profileDescription; ?>" name="description" maxlength="150"></textarea></p>
 		<p><input type="submit" name="submit" value="Spara" /></p>
 	</form>
 </div>
--- php/pages/editProfile.php
+++ php/pages/editProfile.php
@@ -1,6 +1,44 @@
 <?php
 include_once('php/DBQuery.php');
 
+//Current variables
+//Separate file? Combined with php/pages/profile.php?
+$result = DBQuery::sql("SELECT mail FROM user WHERE id = '$_SESSION[user_id]' AND mail  IS NOT NULL");
+if(count($result) == 1)
+	$profileMail = $result[0]["mail"];
+else
+	$profileMail = "";
+
+$result = DBQuery::sql("SELECT phone_number FROM user WHERE id = '$_SESSION[user_id]' AND phone_number IS NOT NULL");
+if(count($result) == 1)
+	$profileNumber = $result[0]["phone_number"];
+else
+	$profileNumber = "";
+
+$result = DBQuery::sql("SELECT ssn FROM user WHERE id = '$_SESSION[user_id]' AND ssn IS NOT NULL");
+if(count($result) == 1)
+	$profileSsn = $result[0]["ssn"];
+else
+	$profileSsn = "";
+
+$result = DBQuery::sql("SELECT name FROM user WHERE id = '$_SESSION[user_id]' AND name IS NOT NULL");
+if(count($result) == 1)
+	$profileName = $result[0]["name"];
+else
+	$profileName = "";
+
+$result = DBQuery::sql("SELECT last_name FROM user WHERE id = '$_SESSION[user_id]' AND last_name IS NOT NULL");
+if(count($result) == 1)
+	$profileLastName = $result[0]["last_name"];
+else
+	$profileLastName = "";
+
+$result = DBQuery::sql("SELECT description FROM user WHERE id = '$_SESSION[user_id]' AND description IS NOT NULL");
+if(count($result) == 1)
+	$profileDescription = $result[0]["description"];
+else
+	$profileDescription = "";
+
 if(isset($_POST['submit'])) {
 	$phoneNumber = $_POST['phone_number'];
 	$ssn = $_POST['ssn'];

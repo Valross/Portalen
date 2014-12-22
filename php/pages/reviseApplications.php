b47fb682f0ebb71771fef1b2f2d2931ceeea626b
Jonathan <jonathan.grangien@gmail.com>
2014-12-23 00:59:40 +0100

    Implement basic display of applicants on revise-applications page

--- php/pages/reviseApplications.php
+++ php/pages/reviseApplications.php
@@ -1,27 +1,31 @@
 <?php
-echo "test";
+$result = DBQuery::sql("SELECT * FROM application");
+$nResults = count($result);
+// echo "reults = " . $nResults;
 
-//fråga
+for($i=0; $i<$nResults; $i++) { 
+	$name = $result[$i]["name"];
+	$lastName = $result[$i]["last_name"];
+	$ssn = $result[$i]["ssn"];
+	$mail = $result[$i]["mail"];
 
-//loop
+	//html lista, knappar etc
+	?>
 
-//var = dskladjs[i][kolumner]
-//...
+	<!--html-->
+	<!-- <link rel="stylesheet" href="css/style.css"> -->
+	<div id="application-container">
+	<ul>
+	<li> <!-- <a href="#"> --> <?php echo $name 	?> <!-- </a> --> </li>
+	<li> <!-- <a href="#"> --> <?php echo $lastName ?> <!-- </a> --> </li>
+	<li> <!-- <a href="#"> --> <?php echo $ssn 	    ?> <!-- </a> --> </li>
+	<li> <!-- <a href="#"> --> <?php echo $mail 	?> <!-- </a> --> </li>
+	</ul>
 
-//html lista, knappar etc
+	</div>
 
-//avslut loop
+	<?php
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
 ?>

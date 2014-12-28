ac09be7922a40e97bd9e205b3df2d2f5300f1df4
Jonathan <jonathan.grangien@gmail.com>
2014-12-28 23:58:40 +0100

    Implement displaying of team names on revise-applications

--- php/pages/reviseApplications.php
+++ php/pages/reviseApplications.php
@@ -1,29 +1,33 @@
 <?php
 $result = DBQuery::sql("SELECT * FROM application");
-$nResults = count($result);
-// echo "reults = " . $nResults;
+$howMany = count($result);
+// echo "reults = " . $howMany;
 
-for($i=0; $i<$nResults; $i++) { 
+for($i=0; $i<$howMany; $i++) { 
 	$name = $result[$i]["name"];
 	$lastName = $result[$i]["last_name"];
 	$ssn = $result[$i]["ssn"];
 	$mail = $result[$i]["mail"];
+	$appId = $result[$i]["id"];
 
-	//html lista, knappar etc
+	$teamIds = DBQuery::sql("SELECT group_id FROM application_group WHERE application_id = '$appId'");
+	$nTeams = count($teamIds);
+
 	?>
-
-	<!--html-->
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
-	<div id="application-container">
-	<ul>
-	<li> <!-- <a href="#"> --> <?php echo $name 	?> <!-- </a> --> </li>
-	<li> <!-- <a href="#"> --> <?php echo $lastName ?> <!-- </a> --> </li>
-	<li> <!-- <a href="#"> --> <?php echo $ssn 	    ?> <!-- </a> --> </li>
-	<li> <!-- <a href="#"> --> <?php echo $mail 	?> <!-- </a> --> </li>
-	</ul>
-
-	</div>
-
+		<div id="application-container">
+		<ul>
+		<li> <?php echo $name 	  ?> </li>
+		<li> <?php echo $lastName ?> </li>
+		<li> <?php echo $ssn 	  ?> </li>
+		<li> <?php echo $mail 	  ?> </li>
+		<li> <?php for($j=0; $j<$nTeams; $j++){ 
+					  $currentId = $teamIds[$j]["group_id"];
+				   	  $teamNames = DBQuery::sql("SELECT name FROM work_group WHERE id ='$currentId' ");
+				   	  $thisTeam = $teamNames[0]["name"];
+				   	  echo $thisTeam . " ";
+				   }?> </li>
+		</ul>
+		</div>
 	<?php
 
 }

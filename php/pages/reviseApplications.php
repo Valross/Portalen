1b44e3270144f438dd5bf86b3d2c666ba2a79954
Jonathan <jonathan.grangien@gmail.com>
2014-12-29 00:43:09 +0100

    Change html element to table, basic css styling

--- php/pages/reviseApplications.php
+++ php/pages/reviseApplications.php
@@ -1,3 +1,12 @@
+<!-- table head -->
+<table style="width:100%" id="application-table">
+	<tr>
+		<th>Namn</th>
+		<th>Personnummer</th>
+		<th>Mail</th>
+		<th>Lag</th>
+	</tr>
+
 <?php
 $result = DBQuery::sql("SELECT * FROM application");
 $howMany = count($result);
@@ -14,7 +23,7 @@ for($i=0; $i<$howMany; $i++) {
 	$nTeams = count($teamIds);
 
 	?>
-		<div id="application-container">
+<!-- 		<div id="application-container">
 		<ul>
 		<li> <?php echo $name 	  ?> </li>
 		<li> <?php echo $lastName ?> </li>
@@ -27,9 +36,27 @@ for($i=0; $i<$howMany; $i++) {
 				   	  echo $thisTeam . " ";
 				   }?> </li>
 		</ul>
-		</div>
+		</div> -->
+			<tr>
+				<td><?php echo $name . " " . $lastName?></td>
+				<td><?php echo $ssn?></td>
+				<td><?php echo $mail?></td>
+				<td><?php   for($j=0; $j<$nTeams; $j++){ 
+								$currentId = $teamIds[$j]["group_id"];
+								$teamNames = DBQuery::sql("SELECT name FROM work_group WHERE id ='$currentId' ");
+								$thisTeam = $teamNames[0]["name"];
+
+								if (!($j == $nTeams - 1))
+									echo $thisTeam . ", ";
+								else
+									echo $thisTeam;
+				    	  	}
+				    ?></td>
+			</tr>
 	<?php
 
 }
 
 ?>
+
+</table>

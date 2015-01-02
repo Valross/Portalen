415e5ba604fb5c3e41f519c30bf264d99503ab3b
Knewtn <email@knewt.se>
2015-01-02 14:27:15 +0100

    Added white boxes to layout, forms restyled

--- pages/editProfile.php
+++ pages/editProfile.php
@@ -1,27 +1,50 @@
-	<div class="col-sm-12">
-		<div class="page-header">
-			<h1>Redigera profil</h1>
+<div class="container-fluid">	
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="page-header">
+			<h1>Inställningar</h1>
+			</div>
 		</div>
-	</div>
-</div> <!-- .row -->
+	</div> <!-- .row -->
 
 <div class="row">
 
-<div class="col-sm-5">
+<div class="col-sm-6">
+	<div class="white-box">
+		<h3>Redigera profil</h3>
 	<form action="" method="post">
-	  <h3>Fyll i de fält du vill uppdatera</h3>
-	  <p>Mobilnummer: <input type="text" placeholder="<?php echo $profileNumber; ?>" name="phone_number" maxlength="15" /></p>
-	  <p>Personnummer: <input type="text" placeholder="<?php echo $profileSsn; ?>" name="ssn" maxlength="10" /></p>
-		<p>Mail: <input type="text" placeholder="<?php echo $profileMail; ?>" name="mail" /></p>
-		<p>Förnamn: <input type="text" placeholder="<?php echo $profileName; ?>" name="name" /></p>
-		<p>Efternamn: <input type="text" placeholder="<?php echo $profileLastName; ?>" name="last_name" /></p>
-		<p>Beskrivning: <textarea rows="4" cols="50" placeholder="<?php echo $profileDescription; ?>" name="description" maxlength="150"></textarea></p>
-		<p><input type="submit" name="changeInfo" value="Spara" /></p>
-		<br/>
-		<h3>Byta lösenord</h3>
-		<p>Nuvarande lösenord: <input type="password" name="old_password" /></p>
-		<p>Nytt lösenord: <input type="password" name="password" /></p>
-		<p>Bekräfta lösenord: <input type="password" name="confirmed_password" /></p>
-		<p><input type="submit" name="changePass" value="Ändra lösenord" /></p>
+		<label for="name">Förnamn</label>
+		<input type="text" value="<?php echo $profileName; ?>" name="name" id="name">
+		<label for="last_name">Efternamn</label>
+		<input type="text" value="<?php echo $profileLastName; ?>" name="last_name" id="last_name">
+	  	<label for="ssn">Personnummer</label>
+	  	<input type="text" value="<?php echo $profileSsn; ?>" name="ssn" id="ssn" maxlength="10">
+	  	<label for="phone_number">Mobilnummer</label>
+	  	<input type="text" value="<?php echo $profileNumber; ?>" name="phone_number" id="phone_number" maxlength="15">
+		<label for="mail">Mailadress</label>
+		<input type="text" value="<?php echo $profileMail; ?>" name="mail" id="mail">
+		
+		<label for="description">Presentation</label>
+		<textarea rows="5" cols="50" value="<?php echo $profileDescription; ?>" name="description" id="description" maxlength="150" class="bottom-border"></textarea>
+			
+		<input type="submit" name="changeInfo" value="Uppdatera profil">
 	</form>
+	</div> <!-- .white-box -->
 </div>
+<div class="col-sm-6">
+	<div class="white-box">
+		<h3>Ändra lösenord</h3>
+	<form action="" method="post">
+		<label for="old_password">Nuvarande lösenord</label>
+		<input type="password" name="old_password" id="old_password">
+		<label for="password">Nytt lösenord</label>
+		<input type="password" name="password" id="password">
+		<label for="confirmed_password">Bekräfta nytt lösenord</label>
+		<input type="password" name="confirmed_password" id="confirmed_password" class="bottom-border">
+		<input type="submit" name="changePass" value="Uppdatera lösenord">
+	</form>
+	</div> <!-- .white-box -->
+</div>
+</div> <!-- .row -->
+
+</div> <!-- .container-fluid -->

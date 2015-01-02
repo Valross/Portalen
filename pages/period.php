42f9a9d0d39d2cb6ec417d7566116df7a8c2ff43
Doodle <sjolinder@hotmail.se>
2015-01-02 19:54:57 +0100

    Started with period.php

---
+++ pages/period.php
@@ -0,0 +1,17 @@
+	<div class="col-sm-12">
+		<div class="page-header">
+			<h1>Hantera Perioder</h1>
+		</div>
+	</div>
+</div> <!-- .row -->
+
+<div class="row">
+	
+<div class="col-sm-5">
+	<form action="" method="post">
+		<p><input type="text" placeholder="Period" name="name"/></p>
+		<p><input type="text" placeholder="Startdatum" name="start_date" /></p>
+		<p><input type="text" placeholder="Slutdatum" name="end_date" /></p>
+		<p><input type="submit" name="submit" value="Skapa period"/></p>
+	</form>
+</div>
---
+++ php/pages/period.php
@@ -0,0 +1,18 @@
+<?php
+include_once('php/DBQuery.php');
+
+if(isset($_POST['submit']))
+{
+	$PeriodName = $_POST['name'];
+	$PeriodStart = $_POST['start_date'];
+	$PeriodEnd = $_POST['end_date'];
+	
+		{
+		DBQuery::sql("INSERT INTO period (name, start_date, end_date)
+							VALUES ($PeriodName', '$PeriodStart', '$PeriodEnd')");
+		?>
+		<?php
+	}
+	
+}
+?>

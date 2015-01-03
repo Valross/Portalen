aad24427e03edbe60a2eab45da3d671e54e10da5
Knewtn <email@knewt.se>
2015-01-03 13:58:09 +0100

    Added styling on createAccount

--- pages/createAccount.php
+++ pages/createAccount.php
@@ -1,3 +1,4 @@
+<div class="row">
 	<div class="col-sm-12">
 		<div class="page-header">
 			<h1>Skapa nytt konto</h1>
@@ -6,15 +7,23 @@
 </div> <!-- .row -->
 
 <div class="row">
-	
-<div class="col-sm-5">
+<div class="col-sm-6">
+	<div class="white-box">
 	<form action="" method="post">
-		<p><input type="text" placeholder="Användarnamn" name="user_name"/></p>
-		<p><input type="password" placeholder="Lösenord" name="password" value="test"/></p>
-		<p><input type="text" placeholder="Personnummer" name="ssn" maxlength="10"/></p>
-		<p><input type="text" placeholder="Mail" name="mail"/></p>
-		<p><input type="text" placeholder="Namn" name="name"/></p>
-		<p><input type="text" placeholder="Efternamn" name="last_name"/></p>
-		<p><input type="submit" name="submit" value="Skapa konto"/></p>
+		<label for="user_name">Användarnamn</label>
+		<input type="text" name="user_name" id="user_name">
+		<label for="password">Lösenord</label>
+		<input type="password" name="password" id="password">
+		<label for="ssn">Personnummer</label>
+		<input type="text" placeholder="ååmmddxxxx" name="ssn" id="ssn" maxlength="10">
+		<label for="mail">Mailadress</label>
+		<input type="text" name="mail" id="mail">
+		<label for="name">Förnamn</label>
+		<input type="text" name="name" id="name">
+		<label for="last_name">Efternamn</label>
+		<input type="text" name="last_name" id="last_name" class="border-bottom">
+		<input type="submit" name="submit" value="Skapa konto">
 	</form>
+	</div> <!-- .white-box -->
+</div>
 </div>

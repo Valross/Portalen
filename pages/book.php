aa5f8a0e054b39dc609cd093b71aa54f6f1083bd
Knewtn <email@knewt.se>
2015-01-13 12:16:05 +0100

    Calendar tweaking

--- pages/book.php
+++ pages/book.php
@@ -1,7 +1,17 @@
 <div class="row">	
-	<div class="col-sm-12">
+	<div class="col-sm-6">
 		<div class="page-header">
 			<h1>Boka pass</h1>
+		</div>
+	</div>
+	<div class="col-sm-6 page-header-right">
+		<div class="pull-right form-inline">
+				<div class="btn-group">
+					<button class="btn btn-page-header" data-calendar-view="year">År</button>
+					<button class="btn btn-page-header active" data-calendar-view="month">Månad</button>
+					<button class="btn btn-page-header" data-calendar-view="week">Vecka</button>
+					<button class="btn btn-page-header" data-calendar-view="day">Dag</button>
+				</div>
 		</div>
 	</div>
 </div> <!-- .row -->
@@ -15,24 +25,13 @@
 </div>
 <div class="col-sm-8">
 	<div class="white-box calendar-box">
-		<div class="" style="border: 1px solid #000; overflow: hidden;">
-		<div class="pull-right form-inline">
-				<div class="btn-group">
-					<button class="btn btn-default" data-calendar-view="year">Year</button>
-					<button class="btn btn-default active" data-calendar-view="month">Month</button>
-					<button class="btn btn-default" data-calendar-view="week">Week</button>
-					<button class="btn btn-default" data-calendar-view="day">Day</button>
-				</div>
-		</div>
-		</div>
-		<div class="calendar-top" style="">
-
-			<h3 style="border: 1px solid #000;"></h3>
+		<div class="calendar-top">
+			<h3></h3>
 			<div class="pull-right form-inline">
 			<div class="btn-group">
-				<button class="btn btn-default" data-calendar-nav="prev"><i class="fa fa-angle-left fa-lg fa-margin-right"></i> Prev</button>
-				<button class="btn btn-default" data-calendar-nav="today">Today</button>
-				<button class="btn btn-default" data-calendar-nav="next">Next <i class="fa fa-angle-right fa-lg fa-margin-left"></i></button>
+				<button class="btn btn-default" data-calendar-nav="prev"><i class="fa fa-angle-left fa-lg fa-margin-right"></i> Föregående</button>
+				<button class="btn btn-default" data-calendar-nav="today">Idag</button>
+				<button class="btn btn-default" data-calendar-nav="next">Nästa <i class="fa fa-angle-right fa-lg fa-margin-left"></i></button>
 			</div>
 			</div>
 		</div>

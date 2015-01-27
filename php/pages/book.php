8c7c3f27d634e6c2a3fc47dd22af0259beee4e76
Jonathan <jonathan.grangien@gmail.com>
2015-01-27 21:22:51 +0100

    Pseudo code for calendar back-end

---
+++ php/pages/book.php
@@ -0,0 +1,17 @@
+<?php
+
+//anslut till databasen
+
+//skapa array
+
+//för varje element:
+	//id (work_slot: id ?)
+	//title (work_slot > event: name)
+	//url?
+	//start (work_slot: start_time)
+	//end (work_slot: end_time)
+
+//encoda till json
+
+
+?>

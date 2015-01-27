5b134bfbd91a1250887c1b712c41954f4e52af40
Jonathan <jonathan.grangien@gmail.com>
2015-01-27 21:51:16 +0100

    Update period date values for 2015, fixes createEvent
    added simple error message to invalid choice of date in createEvent

--- php/pages/createEvent.php
+++ php/pages/createEvent.php
@@ -9,32 +9,40 @@ if(isset($_POST['submit']))
 	$end = $_POST['end'];
 	
 	$periodId = DBQuery::sql("SELECT id FROM period WHERE start_date < '$start' AND end_date > '$start'");
-	$periodId = $periodId[0]['id'];
-	if($name != '' && $type != 'no' && $start != '' && $end != '' && $start < $end)
-	{
-		DBQuery::sql("INSERT INTO event (id, name, event_type_id, start_time, end_time, period_id)
-						VALUES ('', '$name', '$type', '$start', '$end', '$periodId')");
-		$eventId = DBQuery::$lastId;
-		if(isset($_POST['slotGroups']))
+	
+	if(!isset($periodId[0]["id"]))	//date invalid
+    	echo "Error: Det valda datumet har ingen definierad period";
+
+    else{							//date valid
+		$periodId = $periodId[0]["id"];
+		// echo "periodId = " . $periodId;
+
+		if($name != '' && $type != 'no' && $start != '' && $end != '' && $start < $end)
 		{
-			$slotGroups = $_POST['slotGroups'];
-			$slotStarts = $_POST['slotStarts'];
-			$slotEnds = $_POST['slotEnds'];
-			$slotPoints = $_POST['slotPoints'];
-			
-			for($i = 0; $i < count($slotGroups); ++$i)
+			DBQuery::sql("INSERT INTO event (id, name, event_type_id, start_time, end_time, period_id)
+							VALUES ('', '$name', '$type', '$start', '$end', '$periodId')");
+			$eventId = DBQuery::$lastId;
+			if(isset($_POST['slotGroups']))
 			{
-				$groupIds = DBQuery::sql("SELECT id FROM work_group WHERE name = '$slotGroups[$i]'");
-				$groupId = $groupIds[0]['id'];
-				DBQuery::sql("INSERT INTO work_slot (group_id, event_id, start_time, end_time, points)
-						VALUES ('$groupId', '$eventId', '$slotStarts[$i]', '$slotEnds[$i]', '$slotPoints[$i]')");
+				$slotGroups = $_POST['slotGroups'];
+				$slotStarts = $_POST['slotStarts'];
+				$slotEnds = $_POST['slotEnds'];
+				$slotPoints = $_POST['slotPoints'];
+				
+				for($i = 0; $i < count($slotGroups); ++$i)
+				{
+					$groupIds = DBQuery::sql("SELECT id FROM work_group WHERE name = '$slotGroups[$i]'");
+					$groupId = $groupIds[0]['id'];
+					DBQuery::sql("INSERT INTO work_slot (group_id, event_id, start_time, end_time, points)
+							VALUES ('$groupId', '$eventId', '$slotStarts[$i]', '$slotEnds[$i]', '$slotPoints[$i]')");
+				}
 			}
+			?>
+			<script>
+				window.location = "?page=createEvent";
+			</script>
+			<?php
 		}
-		?>
-		<script>
-			window.location = "?page=createEvent";
-		</script>
-		<?php
 	}
 }
 function loadTypes()

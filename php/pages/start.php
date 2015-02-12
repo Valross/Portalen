9961fd9219ea9d2cb6658eedc32292915141e85d
iSirux <joelssonsimon@gmail.com>
2015-02-12 11:28:28 +0100

    Bug fix - om ingen nyhet

--- php/pages/start.php
+++ php/pages/start.php
@@ -6,33 +6,29 @@ include_once('php/DBQuery.php');
 function loadTitle()
 {
 	$groups = DBQuery::sql("SELECT title, date FROM news ORDER BY date");
-	?>
-		<?php echo $groups[count($groups)-1]['title']; ?>
-	<?php
+	if(count($groups) > 0)
+		echo $groups[count($groups)-1]['title'];
 }
 
 function loadMessage()
 {
 	$groups = DBQuery::sql("SELECT message, date FROM news ORDER BY date");
-	?>
-		<?php echo $groups[count($groups)-1]['message']; ?>
-	<?php
+	if(count($groups) > 0)
+		echo $groups[count($groups)-1]['message'];
 }
 
 function loadUser()
 {
 	$groups = DBQuery::sql("SELECT user_id, date FROM news ORDER BY date");
-	?>
-		<?php echo $groups[count($groups)-1]['user_id']; ?>
-	<?php
+	if(count($groups) > 0)
+		echo $groups[count($groups)-1]['user_id'];
 }
 
 function loadDate()
 {
 	$groups = DBQuery::sql("SELECT date FROM news ORDER BY date");
-	?>
-		<?php echo $groups[count($groups)-1]['date']; ?>
-	<?php
+	if(count($groups) > 0)
+		echo $groups[count($groups)-1]['date'];
 }
 
 ?>

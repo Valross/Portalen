d32606d194f169182f8f10cf2a5d83b9cd298169
iSirux <joelssonsimon@gmail.com>
2015-03-15 13:37:28 +0100

    Fixed bug when booking others

--- php/pages/eventBookWorkSlot.php
+++ php/pages/eventBookWorkSlot.php
@@ -7,7 +7,7 @@ if(isset($_GET['event_id']) && isset($_GET['user_id']) && isset($_GET['work_slot
 	$user_id = $_GET['user_id'];
 	$work_slot_id = $_GET['work_slot_id'];
 
-	if($user_id == $_SESSION['user_id'] || $checkAdminAccess())
+	if($user_id == $_SESSION['user_id'] || checkAdminAccess())
 	{
 		DBQuery::sql("INSERT INTO user_work (work_slot_id, user_id, checked)
                                   VALUES ('$work_slot_id', '$user_id', '0')");

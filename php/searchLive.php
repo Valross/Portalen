0735625be7234b6e086cd4e572e12d4f403df651
jonathan <jonathan@localhost.localdomain>
2015-03-17 00:31:31 +0100

    Add max results limit to live results

--- php/searchLive.php
+++ php/searchLive.php
@@ -18,6 +18,7 @@ $searchString = DBQuery::safeString($searchString);
 // Check Length More Than One Character
 if (strlen($searchString) > 1 && $searchString !== ' ') {
 	$noResults = 1;
+	$maxResultsPerCategory = 5;
 
 	// Search users
 	$users = DBQuery::sql("SELECT id, name, last_name, mail FROM user 
@@ -27,14 +28,23 @@ if (strlen($searchString) > 1 && $searchString !== ' ') {
 		$noResults = 0;
 		echo "Användare";
 		
-		for ($i=0; $i < count($users); ++$i) { 
+		for ($i=0; $i < count($users) and $i <= $maxResultsPerCategory; ++$i) { 
 			$userId = $users[$i]['id'];
 
 			$display_name = preg_replace("/".$searchString."/i", "<b class='highlight'>".$searchString."</b>"
 				, $users[$i]['name'] . " " . $users[$i]['last_name']);
 
-			$output = str_replace('nameString', $display_name, $html);
-			$output = str_replace('urlString', "?page=userProfile&id=$userId", $output);
+			if(!($i == $maxResultsPerCategory)){
+				$output = str_replace('nameString', $display_name, $html);
+				$output = str_replace('urlString', "?page=userProfile&id=$userId", $output);
+			}
+
+			// After last result
+			else{
+				$output = str_replace('nameString', "...", $html);
+				$output = str_replace('urlString', "", $output);
+			}
+
 			echo($output);
 	 	} 	
 	}
@@ -46,14 +56,22 @@ if (strlen($searchString) > 1 && $searchString !== ' ') {
 		$noResults = 0;
 		echo "Evenemang";
 		
-		for ($i=0; $i < count($events); ++$i) { 
+		for ($i=0; $i < count($events) and $i <= $maxResultsPerCategory; ++$i) { 
 			$eventId = $events[$i]['id'];
 
 			$display_name = preg_replace("/".$searchString."/i", "<b class='highlight'>".$searchString."</b>"
 				, $events[$i]['name']);
 
-			$output = str_replace('nameString', $display_name, $html);
-			$output = str_replace('urlString', "?page=event&id=$eventId", $output);
+			if(!($i == $maxResultsPerCategory)){
+				$output = str_replace('nameString', $display_name, $html);
+				$output = str_replace('urlString', "?page=event&id=$eventId", $output);
+			}
+
+			// After last result
+			else{
+				$output = str_replace('nameString', "...", $html);
+				$output = str_replace('urlString', "", $output);
+			}
 			echo($output);
 	 	} 	
 	}
@@ -65,14 +83,22 @@ if (strlen($searchString) > 1 && $searchString !== ' ') {
 		$noResults = 0;
 		echo "Lag";
 		
-		for ($i=0; $i < count($teams); ++$i) { 
+		for ($i=0; $i < count($teams) and $i <= $maxResultsPerCategory; ++$i) { 
 			$teamId =  $teams[$i]['id'];
 
 			$display_name = preg_replace("/".$searchString."/i", "<b class='highlight'>".$searchString."</b>"
 				, $teams[$i]['name']);
 
-			$output = str_replace('nameString', $display_name, $html);
-			$output = str_replace('urlString', "?page=group&id=$teamId", $output);
+			if(!($i == $maxResultsPerCategory)){
+				$output = str_replace('nameString', $display_name, $html);
+				$output = str_replace('urlString', "?page=group&id=$teamId", $output);
+			}
+
+			// After last result
+			else{
+				$output = str_replace('nameString', "...", $html);
+				$output = str_replace('urlString', "", $output);
+			}
 			echo($output);
 	 	} 	
 	}

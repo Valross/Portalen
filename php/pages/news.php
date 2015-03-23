ac6cc79cb604508d359b8a19e0203e0a75c1ac23
iSirux <joelssonsimon@gmail.com>
2015-03-23 15:29:11 +0100

    Page system for news

--- php/pages/news.php
+++ php/pages/news.php
@@ -5,19 +5,93 @@ function loadAll()
 {
 	$news = DBQuery::sql("SELECT id, date FROM news ORDER BY date DESC");
 
-	for($i = 0; $i < count($news); ++$i)
+	if(isset($_GET['pageNumber']))
+		$currentPage = $_GET['pageNumber'];
+	else
+		$currentPage = 0;
+
+	$itemsPerPage = 4;
+	$totalItems = count($news);
+	$lastPage = floor(($totalItems / $itemsPerPage));
+	$startItem = $currentPage * $itemsPerPage;
+
+	if($currentPage <= $lastPage)
 	{
-		echo '<div class="col-sm-7">
-				<div class="white-box">';
-					echo '<h2>'.loadTitle($news[$i]['id']).'</h2>';
-					echo '<div class="news-info"><span>';
-					echo loadUserAvatar($news[$i]['id']);
-					echo loadUserName($news[$i]['id']);
-					echo '</span> <span class="time">- '.loadDate($news[$i]['id']).'</span>'.loadRemove($news[$i]['id']).'</div>';
-					echo '<p>'.loadMessage($news[$i]['id']).'</p>';
-		echo    '</div>
-			 </div>';
+		for($i = $startItem; $i < $startItem + $itemsPerPage && $i < count($news); ++$i)
+		{
+			echo '<div class="col-sm-7">
+					<div class="white-box">';
+						echo '<h2>'.loadTitle($news[$i]['id']).'</h2>';
+						echo '<div class="news-info"><span>';
+						echo loadUserAvatar($news[$i]['id']);
+						echo loadUserName($news[$i]['id']);
+						echo '</span> <span class="time">- '.loadDate($news[$i]['id']).'</span>'.loadRemove($news[$i]['id']).'</div>';
+						echo '<p>'.loadMessage($news[$i]['id']).'</p>';
+			echo    '</div>
+				 </div>';
+		}
+		loadPageNumbers($currentPage, $lastPage);
+	}		
+}
+	
+function loadPageNumbers($currentPage, $lastPage)
+{
+	echo '<div class="col-sm-7">
+					<div class="white-box">';
+	echo '<p>';
+
+	if($currentPage == 0)
+	{
+		echo 'Första ';
+		echo '<a href="?page=news&pageNumber='.($currentPage+1).'">'.($currentPage+1).'</a> ';
+
+		if($lastPage > $currentPage+2)
+			echo '... ';
+
+		if($lastPage > $currentPage+2)
+			echo '<a href="?page=news&pageNumber='.($lastPage).'">Sista</a> ';
 	}
+	else if($currentPage == $lastPage)
+	{
+		echo '<a href="?page=news&pageNumber=0">Första</a> ';
+
+		if($currentPage-2 > 0)
+			echo '... ';
+
+		echo '<a href="?page=news&pageNumber='.($currentPage-1).'">'.($currentPage-1).'</a> ';
+		
+		echo 'Sista ';
+	}
+	else
+	{
+		echo '<a href="?page=news&pageNumber=0">Första</a> ';
+
+		if($currentPage-2 > 0)
+			echo '... ';
+
+		if($currentPage-1 > 0)
+			echo '<a href="?page=news&pageNumber='.($currentPage-1).'">'.($currentPage-1).'</a> ';
+		
+		echo $currentPage.' ';
+
+		if($currentPage < $lastPage)
+		{
+			if($currentPage == $lastPage-1)
+				echo '<a href="?page=news&pageNumber='.($currentPage+1).'">Sista</a> ';
+			else
+				echo '<a href="?page=news&pageNumber='.($currentPage+1).'">'.($currentPage+1).'</a> ';
+		}
+
+		if($lastPage > $currentPage+2)
+			echo '... ';
+		
+		if($lastPage > $currentPage+1)
+			echo '<a href="?page=news&pageNumber='.($lastPage).'">Sista</a> ';
+	}
+
+	echo '</p>';
+	echo    '</div>
+		 </div>';
 }
 
 function loadTitle($news_id)

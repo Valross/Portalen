9e1858131a61b548c0b8f5cb8adbca785dc55b5d
jonathan <jonathan.grangien@gmail.com>
2015-03-29 19:48:03 +0200

    Implement pagination for events and teams too
    names only

--- php/pages/searchPage.php
+++ php/pages/searchPage.php
@@ -5,10 +5,18 @@ loadTitleForBrowser('Sökresultat');
 	function loadAllResults($searchString){
 		
 		// Get results
-		$users = DBQuery::sql("SELECT id, name, last_name, mail FROM user 
-	   			 WHERE name LIKE '%" . $searchString . "%' OR last_name LIKE '%" . $searchString  ."%'");
+		// $users = DBQuery::sql("SELECT id, name, last_name, mail FROM user 
+	 //   			 WHERE name LIKE '%" . $searchString . "%' OR last_name LIKE '%" . $searchString  ."%'");
 		// $events = DBQuery::sql("SELECT id, name, start_time FROM event WHERE name LIKE '%" . $searchString . "%'");
 		// $teams = DBQuery::sql("SELECT id, name FROM work_group WHERE name LIKE '%" . $searchString . "%'");
+
+		$results = DBQuery::sql("SELECT name FROM user WHERE name LIKE '%" . $searchString . "%' OR last_name LIKE '%" . $searchString  ."%'
+			UNION
+			SELECT name FROM event WHERE name LIKE '%" . $searchString . "%' 
+			UNION
+			SELECT name FROM work_group WHERE name LIKE '%" . $searchString . "%' ");
+
+		echo "DEBUG: results = " . count($results);
 
 		$currentpage = 0;
 
@@ -16,7 +24,7 @@ loadTitleForBrowser('Sökresultat');
 			$currentPage = $_GET['pageNumber'];
 
 		$itemsPerPage = 3;
-		$totalItems = count($users);
+		$totalItems = count($results);
 		$lastPage = ceil(($totalItems / $itemsPerPage))-1;
 		$startItem = $currentPage * $itemsPerPage;
 
@@ -26,10 +34,40 @@ loadTitleForBrowser('Sökresultat');
 
 		if($currentPage <= $lastPage) {
 			// echo "hej";
-			echo "users = " . count($users);
+			// echo "users = " . count($users);
 
-			for($i = $startItem; $i < $startItem + $itemsPerPage; ++$i) {
-	        	echo ", i = " . $i;
+			for($i = $startItem; $i < $startItem + $itemsPerPage && $i < count($results); ++$i) {
+	     //    	$firstName = $results[$i]['name']; 
+	     //    	$lastName = $results[$i]['last_name']; 
+	     //    	$userId = $results[$i]['id']; 
+	     //    	$userMail = $results[$i]['mail']; 
+
+	  			// echo "<ul>\n"; 
+	  			// echo "<li>" . "<a href=\"?page=userProfile&id=$userId\">" . $firstName . " " . $lastName . "</a></li>\n"; 
+	  			// echo "<li>" . "<a href=mailto:" . $userMail . ">" . $userMail . "</a></li>\n"; 
+	  			// echo "</ul>"; 
+
+	  			$name = $results[$i]['name'];
+	  			echo "<p>".$name."</p>";
+  			}
+
+				loadPageNumbers($currentPage, $lastPage, 'searchPage', '&query='.$searchString);
+				
+				// echo '<p>Användare: </p>';
+				// displayUsers($searchString, $users, $startItem, $itemsPerPage);
+
+				// echo '<p>Evenemang: </p>';
+				// displayEvents($searchString, $events, $startItem, $itemsPerPage);
+				// echo '<p>Lag: </p>';
+				// displayTeams($searchString, $teams, $startItem, $itemsPerPage);
+		
+		}
+			
+
+	}
+
+	function displayUsers($searchString, $users, $startItem, $itemsPerPage){
+		for($i = $startItem; $i < $startItem + $itemsPerPage && $i < count($users); ++$i) {
 	        	$firstName = $users[$i]['name']; 
 	        	$lastName = $users[$i]['last_name']; 
 	        	$userId = $users[$i]['id']; 
@@ -39,40 +77,11 @@ loadTitleForBrowser('Sökresultat');
 	  			echo "<li>" . "<a href=\"?page=userProfile&id=$userId\">" . $firstName . " " . $lastName . "</a></li>\n"; 
 	  			echo "<li>" . "<a href=mailto:" . $userMail . ">" . $userMail . "</a></li>\n"; 
 	  			echo "</ul>"; 
-	  		}
-
-				loadPageNumbers($currentPage, $lastPage, 'searchPage', '&query='.$searchString);
-				
-				// echo '<p>Användare: </p>';
-				// displayUsers($searchString, $users);
-
-				// echo '<p>Evenemang: </p>';
-				// displayEvents($searchString, $events);
-
-				// echo '<p>Lag: </p>';
-				// displayTeams($searchString, $teams);
-		
-		}
-			
-
-	}
-
-	function displayUsers($searchString, $users){
-		for($i=0; $i < count($users); ++$i){ 
-        	$firstName = $users[$i]['name']; 
-        	$lastName = $users[$i]['last_name']; 
-        	$userId = $users[$i]['id']; 
-        	$userMail = $users[$i]['mail']; 
-
-  			echo "<ul>\n"; 
-  			echo "<li>" . "<a href=\"?page=userProfile&id=$userId\">" . $firstName . " " . $lastName . "</a></li>\n"; 
-  			echo "<li>" . "<a href=mailto:" . $userMail . ">" . $userMail . "</a></li>\n"; 
-  			echo "</ul>"; 
   		}
   	}
 
-  	function displayEvents($searchString, $events){
-	   	for($i=0; $i < count($events); ++$i){ 
+  	function displayEvents($searchString, $events, $startItem, $itemsPerPage){
+	   	for($i = $startItem; $i < $startItem + $itemsPerPage && $i < count($events); ++$i) {
         	$eventName = $events[$i]['name'];
         	$eventId = $events[$i]['id'];
         	$date = $events[$i]['start_time'];
@@ -83,7 +92,7 @@ loadTitleForBrowser('Sökresultat');
   		}
   	}
 
-  	function displayTeams($searchString, $teams){
+  	function displayTeams($searchString, $teams, $startItem, $itemsPerPage){
 	   	for($i=0; $i < count($teams); ++$i){ 
         	$teamName = $teams[$i]['name'];
         	$teamId =  $teams[$i]['id'];

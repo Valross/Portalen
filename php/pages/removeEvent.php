721de2384293a8a0f32564f293a607f798c31119
iSirux <joelssonsimon@gmail.com>
2015-05-06 15:04:04 +0200

    Removal of events

---
+++ php/pages/removeEvent.php
@@ -0,0 +1,27 @@
+<?php
+include_once('php/DBQuery.php');
+
+if(isset($_GET['event_id']) && checkAdminAccess() == 1)
+{
+	$event_id = $_GET['event_id'];
+
+	DBQuery::sql("DELETE FROM event
+        					WHERE '$event_id' = id");
+
+	?>
+		<script>
+			window.location = "?page=book";
+		</script>
+	<?php
+}
+else
+{
+	?>
+		<script>
+			window.location = "?page=book";
+			alert("Något gick fel!")
+		</script>
+	<?php
+}
+
+?>

cd28608567477aff96cfcc3f2934e6755ed6f677
jonathan <jonathan.grangien@gmail.com>
2015-05-19 18:36:17 +0200

    add avatar and link to logged in users

--- php/getLoggedInUsers.php
+++ php/getLoggedInUsers.php
@@ -1,14 +1,18 @@
 <?php
 
-include_once($_SERVER['DOCUMENT_ROOT'] . '/Portalen/php/DBQuery.php');
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . '/Portalen/php/general.php');
 
-// get logged in users
+// get logged in users (2 min window)
 $users = DBQuery::sql("SELECT id, name, latest_session 
 					   FROM user 
 					   WHERE latest_session > '".date('Y-m-d H:i:s',strtotime('-2 min'))."'");
 
 for ($i=0; $i < count($users); ++$i) { 
-	echo '<p>' . $users[$i]["name"] . '</p>';
+	$id = $users[$i]["id"];
+	$name = $users[$i]["name"];
+
+	echo "<p> <a href=\"?page=userProfile&id=$id\">" . loadAvatarFromUser($id, 32) . $users[$i]["name"] . "</a></p>";
 }
 
 

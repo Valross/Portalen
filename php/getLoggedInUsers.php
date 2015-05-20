38ca417d5b4d702253f4833119a1c07bf378a541
Jonathan <jonathan.grangien@gmail.com>
2015-05-21 00:31:34 +0200

    change user logged in window to 30 sec

--- php/getLoggedInUsers.php
+++ php/getLoggedInUsers.php
@@ -3,10 +3,10 @@
 session_start();
 include_once($_SERVER['DOCUMENT_ROOT'] . '/Portalen/php/general.php');
 
-// get logged in users (2 min window)
+// get logged in users (30 sec window)
 $users = DBQuery::sql("SELECT id, name, last_name, latest_session 
 					   FROM user 
-					   WHERE latest_session > '".date('Y-m-d H:i:s',strtotime('-2 min'))."'");
+					   WHERE latest_session > '".date('Y-m-d H:i:s',strtotime('-30 sec'))."'");
 
 for ($i=0; $i < count($users); ++$i) { 
 	$id = $users[$i]["id"];

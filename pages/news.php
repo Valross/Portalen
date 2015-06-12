eda29263fc24a37ef634af10c02a12248ab35925
iSirux <joelssonsimon@gmail.com>
2015-06-12 13:43:42 +0200

    Buttons added

--- pages/news.php
+++ pages/news.php
@@ -1,7 +1,15 @@
 <div class="row">
-	<div class="col-sm-12">
+	<div class="col-sm-6">
 		<div class="page-header">
 			<h1><span class="fa fa-newspaper-o fa-fw"></span> Nyheter</h1>
+		</div>
+	</div>
+	<div class="col-sm-6 page-header-right">
+		<div class="pull-right form-inline">
+			<div class="btn-group">
+				<a href="?page=createNews" class="btn btn-page-header"><span class="fa fa-pencil fa-fw fa-lg"></span>Skriv nyhet</a>
+				<a href="?page=news" class="btn btn-page-header active"><span class="fa fa-newspaper-o fa-fw fa-lg"></span>Nyheter</a>
+			</div>
 		</div>
 	</div>
 </div> <!-- .row -->

441523afdc4ca2cdb442d1313fbdf09f1cb7ad7b
iSirux <joelssonsimon@gmail.com>
2015-06-12 14:19:33 +0200

    Added buttons

--- pages/staffList.php
+++ pages/staffList.php
@@ -1,9 +1,16 @@
 <div class="row">
-	<div class="col-sm-12">
+	<div class="col-sm-6">
 		<div class="page-header">
 			<h1><span class="fa fa-list fa-fw fa-lg"></span> Personallista
-				 - <a href="?page=mailList"><span class="fa fa-envelope-o fa-fw fa-lg"></span>Maillista</a>
 			</h1>
+		</div>
+	</div>
+	<div class="col-sm-6 page-header-right">
+		<div class="pull-right form-inline">
+			<div class="btn-group">
+				<a href="?page=staffList" class="btn btn-page-header active"><span class="fa fa-list fa-fw fa-lg"></span>Personallista</a>
+				<a href="?page=mailList" class="btn btn-page-header"><span class="fa fa-envelope-o fa-fw fa-lg"></span>Maillista</a>
+			</div>
 		</div>
 	</div>
 </div>
